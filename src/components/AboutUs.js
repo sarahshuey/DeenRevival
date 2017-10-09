bfd3a9b11e1270ac0896124781bfbaa192a35fba
Sarah Shuey <sarah.shuey@me.com>
2017-10-09 12:51:42 -0500

    about us page in progress

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import '../styles/App.css';
+import image from '../Images/Austin.jpg'
+
 
 class AboutUs extends Component {
   render() {
+    let aboutStyle={
+      display:"flex",
+      flexDirection:"row"
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">About Us</h1>
+        <header className="header">
+          <h1 className="title">About Us</h1>
         </header>
+        <div className="page" style={aboutStyle}>
+        <div className="AustinPic">
+        <img style={{width: 550, padding:"10px"}} src={image} alt="Austin Deen"></img>
+        </div>
+        <div className="AboutAustin">
+        <h2> Austin Deen </h2>
+        <p> Austin Deen has been in construction for ----- years. He first got into construction when he was --- years old. Austin specializes in high end remodels with a focus on kitchens and bathrooms.This needs to be much longer add stuff about Texas State, and past work history.
+        </p>
+        </div>
+      </div>
       </div>
     );
   }
