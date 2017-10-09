d395ab87ec2fe12c39c4d5dc88864ab6fdf6b0fe
Sarah Shuey <sarah.shuey@me.com>
2017-10-09 12:55:16 -0500

    basic contact us page

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -5,9 +5,18 @@ class ContactUs extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">ContactUs</h1>
+        <header className="header">
+          <h1 className="title">ContactUs</h1>
         </header>
+        <div className="info">
+        <p>
+        You can reach us by email any time or call us Monday - Friday between 9:00 - 5:00
+        <br></br>
+        Phone: (512) 417-7859
+        <br></br>
+        Email: deen.austin@gmail.com
+        </p>
+        </div>
       </div>
     );
   }
